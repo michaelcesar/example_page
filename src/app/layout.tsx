9558adc6bf3897734689f222a2c3d687218719d1
michaelcesar <michaelcesarflopesgit@gmail.com>
2024-10-03 01:04:54 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+
+const Poppins = localFont({
+  src: [
+    {
+      path: './fonts/poppins-300.woff2',
+      weight: '300',
+      style: 'normal'
+    },
+    {
+      path: './fonts/poppins-400.woff2',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: './fonts/poppins-500.woff2',
+      weight: '500',
+      style: 'normal'
+    },
+    {
+      path: './fonts/poppins-600.woff2',
+      weight: '600',
+      style: 'normal'
+    },
+    {
+      path: './fonts/poppins-700.woff2',
+      weight: '700',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-poppins'
+});
+
+export const metadata: Metadata = {
+  title: 'Arco Content',
+  description: 'Arco Content'
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${Poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
